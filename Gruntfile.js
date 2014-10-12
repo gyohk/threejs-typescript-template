75bea7f2d4d4414d3c3db996b1d9003ca00c3157
satoru kimura <s_kimura@sa.il24.net>
2014-10-12 20:43:07 +0900

    inital commit.

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        // package.json の内容を読み込み。他の箇所から参照できるようにする。
+        pkg: grunt.file.readJSON('package.json'),
+        
+        // 設定値の定義。名前が opt であることに特別な理由はない。
+        opt: {
+            "tsDir": "src",
+            "outDir": "dest"
+        },
+        // grunt-ts に対する設定
+        ts: {
+            options: {
+                target: 'es5', // --target に指定するパラメータ
+                module: 'commonjs', // --module に指定するパラメータ
+                noImplicitAny: true // --noImplicitAny を使うか使わないか
+            },
+            // main の動作を定義。名前は main 以外のものでも問題ない。
+            // test という名前でテストコードのコンパイル処理を追加することが多い。
+            main: {
+                src: ['<%= opt.tsDir %>/*.ts'],
+                outDir: '<%= opt.outDir %>/js'
+            }
+        },
+        tsd: {
+            refresh: {
+                options: {
+                    // execute a command
+                    command: 'reinstall',
+                    
+                    //optional: always get from HEAD
+                    latest: true,
+                    
+                    // specify config file
+                    config: './conf/tsd.json',
+                    
+                    // experimental: options to pass to tsd.API
+                    opts: {
+                        // props from tsd.Options
+                    }
+                }
+            }
+        },
+        tslint: {
+            options: {
+                configuration: grunt.file.readJSON("./conf/tslint.json")
+            },
+            files: {
+                src: ['<%= opt.tsDir %>/Main.ts']
+            }
+        },
+        copy: {
+            app: {
+                files: [
+                    {expand: true, cwd: 'skeleton/', src: ['**'], dest: '<%= opt.outDir %>/'}
+                ]
+            }
+        },
+        clean: {
+            build: {
+                src: [
+                    '<%= opt.outDir %>/**/*'
+                ]
+            }
+        },
+    });
+    
+    grunt.registerTask('setup', ['clean', 'tsd']);
+    grunt.registerTask('default', ['clean', 'copy', 'ts', 'tslint']);
+    
+    // 
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-tsd');
+    grunt.loadNpmTasks('grunt-tslint');
+};
