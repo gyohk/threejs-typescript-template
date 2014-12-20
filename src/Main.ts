18aebf9ee740a335e08a5273f05ce50dee0d9aae
satoru kimura <s_kimura@sa.il24.net>
2014-12-20 11:36:51 +0900

    moved from other repo.

--- src/Main.ts
+++ src/Main.ts
@@ -8,38 +8,109 @@ module app {
         private scene:THREE.Scene;
         private camera:THREE.PerspectiveCamera;
 
-        private cube:THREE.Mesh;
+        private material:THREE.ShaderMaterial;
+        private geometry:THREE.Geometry;
+
+        private vs_source: string;
+        private fs_source: string;
+
+        private baseTime: number;
 
         constructor() {
+            this.baseTime = +new Date;
+            this.loadShader();
+        }
+
+        private loadShader():void {
+            // シェーダーの読み込み
+            $.ajax({
+                ddd: "abc",
+                async: false,
+                url: "./shader.vs",
+                dataType: "text"
+            }).done((data: string) => {
+                this.vs_source = data;
+
+                $.ajax({
+                    async: false,
+                    url: './shader.fs',
+                    dataType: "text"
+                }).done((data: string) => {
+                     this.fs_source = data;
+                     this.init();
+                });
+            });
+        }
+
+        private init():void {
             this.initTHREE();
             this.initScene();
             this.render();
         }
 
         private initTHREE():void {
-            this.scene = new THREE.Scene();
-            this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-            this.scene.add(this.camera);
+            // レンダラの初期化
             this.renderer = new THREE.WebGLRenderer({antialias: true});
-            this.renderer.setSize(window.innerWidth, window.innerHeight);
+            this.renderer.setSize(500, 500);
+            this.renderer.setClearColor(0x000000, 1);
             document.getElementById("container").appendChild(this.renderer.domElement);
+
+            this.scene = new THREE.Scene();
+
+            // カメラの作成
+            this.camera = new THREE.PerspectiveCamera(70, 500 / 500);
+            this.camera.position.set(0, 8, 12);
+            this.camera.lookAt(new THREE.Vector3(-1, 0, 4));
+            this.scene.add(this.camera);
         }
 
         private initScene():void {
-            var geometry = new THREE.BoxGeometry(1, 1, 1);
-            var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-            this.cube = new THREE.Mesh(geometry, material);
-            this.scene.add(this.cube);
+            // (2) マテリアルの作成
+            var texture = THREE.ImageUtils.loadTexture("images/particle.png");
+            this.material = new THREE.ShaderMaterial({
+                vertexShader: this.vs_source,
+                fragmentShader: this.fs_source,
+                uniforms: {
+                    time: {type: "f", value: 0},
+                    size: {type: "f", value: 0.13},
+                    color: {type: "c", value: new THREE.Color(0xffcc88)},
+                    texture: {type: "t", value: texture}
+                },
+                attributes: {
+                    lifetime: {type: "f", value: []},
+                    shift: {type: "f", value: []}
+                },
 
-            this.camera.position.z = 5;
+                // 通常マテリアルのパラメータ
+                blending: THREE.AdditiveBlending, transparent: true, depthTest: false
+            });
+
+            // (3) 形状データを作成（同時に追加の頂点情報を初期化）
+            this.geometry = new THREE.Geometry();
+            var attributes = this.material.attributes;
+            var numParticles = 100000;
+            for (var i = 0; i < numParticles; i++) {
+                var a = Math.PI * 2 * Math.random();
+                var d = 8 + Math.random() * 8;
+                this.geometry.vertices.push(new THREE.Vector3(
+                    Math.sin(a) * d, 3 + Math.random() * 2, Math.cos(a) * d));
+
+                // 追加の頂点情報を初期化
+                attributes.lifetime.value.push(3 + Math.random());
+                attributes.shift.value.push(Math.random());
+            }
+
+            // 物体を作成
+            var mesh = new THREE.PointCloud(this.geometry, this.material);
+            mesh.position.set(0, 0, 0);
+            mesh.sortParticles = false;
+            this.scene.add(mesh);
         }
 
         public render():void {
             requestAnimationFrame(() => this.render());
 
-            this.cube.rotation.x += 0.1;
-            this.cube.rotation.y += 0.1;
-
+            this.material.uniforms.time.value = (+new Date - this.baseTime) / 1000;
             this.renderer.render(this.scene, this.camera);
         }
     }
